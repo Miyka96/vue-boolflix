216651b00e68953cb0caa1d8bc734466b8a6014f
Miyka96 <micaelaseminara@outlook.it>
2022-03-15 18:00:33 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-
+    <SearchNav/>
+    <Main-vue/>
   </div>
 </template>
 
 <script>
+import MainVue from './components/MainVue.vue';
+import SearchNav from './components/SearchNav.vue';
+
 
 
 export default {
   name: 'App',
   components: {
-
+    MainVue,
+    SearchNav,
   }
 }
 </script>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Signika:wght@300&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Signika', sans-serif;
+}
+
+#app{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
 
 </style>
---
+++ src/components/SearchNav.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav>
+    <h1>Boolflix</h1>
+    <div id="search">
+      <input type="search" id="search-bar" />
+      <button id="search-btn">Search</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SearchNav",
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 100%;
+  height: 70px;
+  background-color: rgb(41, 41, 41);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    color: red;
+    font-size: 40px;
+    padding: 10px 40px;
+    font-weight: 800;
+  }
+
+  #search {
+    padding: 10px 100px;
+
+    #search-bar {
+      min-width: 150px;
+      height: 25px;
+      border: 1px solid black;
+      background-color: grey;
+
+      &:hover, &:focus{
+        filter: brightness(1.5);
+      }
+    }
+
+    #search-btn {
+      height: 25px;
+      border: 1px solid black;
+      background-color: rgb(167, 167, 167);
+      padding: 0 10px;
+      margin: 0 5px;
+      cursor: pointer;
+
+      &:hover {
+        filter: brightness(1.5);
+      }
+    }
+  }
+}
+</style>
