f1d54a32fbb2ea7ae2aa834ef315d5313a466b82
Miyka96 <micaelaseminara@outlook.it>
2022-03-15 18:19:18 +0100

    main component

--- src/components/MainVue.vue
+++ src/components/MainVue.vue
@@ -1,6 +1,17 @@
 <template>
   <main>
-    
+    <div class="card">
+
+      <div class="card-thumb"></div>
+
+      <div class="card-description">
+        <h4 id="title"> Titolo: <span>Pierino</span></h4>
+        <h4 id="title-original"> Titolo originale: <span>Pierino al mare</span></h4>
+        <p id="langage">Italo-americano</p>
+        <p id="vote">Voto: 44/10</p>
+
+      </div>
+    </div>
   </main>
 </template>
 
@@ -11,14 +22,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
+  width: 100%;
+  flex-grow: 1;
+  background-color: rgb(27, 27, 27);
+  overflow-y: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 40px;
+  gap: 20px;
 
-main{
+  .card {
+    width: calc(100% / 6);
+    aspect-ratio: 1/2;
+ 
+    display: flex;
+    flex-direction: column;
+    padding: 5px;
+    gap: 10px;
 
-width: 100%;
-flex-grow: 1;
-background-color: rgb(27, 27, 27);
-overflow-y: auto;
+    .card-thumb{
+        flex-grow: 1;
+        border: 1px solid white;
+    }
 
+    .card-description{
+        color: white;
+    }
+  }
 }
-
 </style>
