70fc2940642e28a90b0eea26111e24ddbc9b0019
Miyka96 <micaelaseminara@outlook.it>
2022-03-15 18:01:38 +0100

    navbar

---
+++ src/components/MainVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainVue",
+};
+</script>
+
+<style lang="scss" scoped>
+
+main{
+
+width: 100%;
+flex-grow: 1;
+background-color: rgb(27, 27, 27);
+overflow-y: auto;
+
+}
+
+</style>
