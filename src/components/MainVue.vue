7d3114d8feac4704519103f874926ce6ef5f1b3d
Miyka96 <micaelaseminara@outlook.it>
2022-03-16 01:20:45 +0100

    language flags done

--- src/components/MainVue.vue
+++ src/components/MainVue.vue
@@ -1,15 +1,48 @@
 <template>
   <main>
-    <div class="card"  v-for="el in arrayFilm" :key="el.id">
+    <div class="card" v-for="el in arrayFilm" :key="el.id">
       <div class="card-thumb"></div>
 
       <div class="card-description">
-        <h4 id="title">Titolo: <span>{{ el.title}}</span></h4>
-        <h4 id="title-original">
-          Titolo originale: <span>{{el.original_title}}</span>
+        <h4 id="title">
+          Titolo: <span>{{ el.title }}</span>
         </h4>
-        <p id="langage">{{el.original_language}}</p>
-        <p id="vote">{{el.vote_average}}/10</p>
+        <h4 id="title-original">
+          Titolo originale: <span>{{ el.original_title }}</span>
+        </h4>
+
+        <p v-if="el.original_language == 'it'" id="langage">
+          <img
+            id="flag"
+            src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f1ee-1f1f9.png"
+            alt=""
+          />
+        </p>
+        <p v-else-if="el.original_language == 'en'" id="langage">
+          <img
+            id="flag"
+            src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f1ec-1f1e7.png"
+            alt=""
+          />
+        </p>
+        <p v-else-if="el.original_language == 'fr'" id="langage">
+          <img
+            id="flag"
+            src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f1eb-1f1f7.png"
+            alt=""
+          />
+        </p>
+        <p v-else-if="el.original_language == 'de'" id="langage">
+          <img
+            id="flag"
+            src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f1e9-1f1ea.png"
+            alt=""
+          />
+        </p>
+
+        <p v-else id="langage">Language: {{ el.original_language }}</p>
+
+        <p id="vote">{{ el.vote_average }}/10</p>
       </div>
     </div>
   </main>
@@ -19,11 +52,11 @@
 export default {
   name: "MainVue",
   props: {
-    arrayFilm:{
+    arrayFilm: {
       type: Object,
       required: true,
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -68,6 +101,10 @@ main {
 
       & p {
         font-size: 14px;
+      }
+
+      #flag {
+        height: 20px;
       }
     }
   }
